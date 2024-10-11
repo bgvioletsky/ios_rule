24375e75ce83b42c4fe43056c00dc9bd9f1b5a0e
bgcode <violetcode@icloud.com>
2024-10-11 22:05:00 +0800

    up

--- Loon/Script/xbs_Unlock.js
+++ Loon/Script/xbs_Unlock.js
@@ -2,7 +2,7 @@
  * @Author: bgcode violetcode@icloud.com
  * @Date: 2024-10-11 21:17:59
  * @LastEditors: bgcode violetcode@icloud.com
- * @LastEditTime: 2024-10-11 22:00:48
+ * @LastEditTime: 2024-10-11 22:03:51
  * @FilePath: /ios_rule/Loon/Script/xbs_Unlock.js
  * @Description: 用于重写js脚本
  */
@@ -11,14 +11,31 @@ const myRequest = {
     url: url
 };
 
+function getBaseDoneHeaders(mixHeaders = {}) {
+    return Object.assign({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    },
+    mixHeaders
+)
+}
+
+function getHtmlDoneHeaders() {
+    return getBaseDoneHeaders({
+        'Content-Type': 'text/html;charset=UTF-8'
+    })
+}
 
 $httpClient.get(myRequest).then(xx => {
-    headers = xx.headers;   
-    $done( {response: {
-        status: 200,
-        headers,
-        body: xx.body
-    }});
+    const headers = getHtmlDoneHeaders()
+    $done( {
+        response: {
+            status: 200,
+            headers,
+            body: bg.html
+        }
+    });
 }, reason => {
     $done();
 });
